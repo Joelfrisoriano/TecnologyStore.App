74d5374791d7944b5d2835c2418189adfa6f34ed
Argely <chaelgarciaone@gmail.com>
2024-03-12 21:47:54 -0400

    Probando funcionalidades base de dato

--- TecnologyStoreApp/Data/DetallesFactura.cs
+++ TecnologyStoreApp/Data/DetallesFactura.cs
@@ -9,12 +9,10 @@ namespace TecnologyStoreApp.Data
         public string nombreCliente { get; set; }
         public string direccionEntrega { get; set; }
         public string nombreProductoSeleccionado { get; set; }
-
         public string colorSeleccionado { get; set; }
         public int cantidadSeleccionada { get; set; }
         public decimal precioProductoSeleccionado { get; set; }
         public decimal totalApagar { get; set; }
-
         public string metodoPago { get; set; }
 
     }
